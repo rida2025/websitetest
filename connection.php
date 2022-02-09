439f90e54933c621884e4a376827b323a6cba600
mohammedreda <ridaeljirari@gmail.com>
2022-02-09 23:33:35 +0100

    never give up 3.6.1

--- connection.php
+++ connection.php
@@ -12,7 +12,7 @@ $charset = 'utf8mb4';
 $pdo = "mysql:host=$host;dbname=$db;charset=$charset";
 
 try {
-    $conn = new PDO($conn, $username, $password);
+    $conn = new PDO($pdo, $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
