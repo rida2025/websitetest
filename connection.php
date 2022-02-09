ba1da4c282e05be3d89448606860c27f9b800497
mohammedreda <ridaeljirari@gmail.com>
2022-02-09 17:49:41 +0100

    god please

--- connection.php
+++ connection.php
@@ -3,19 +3,19 @@
 //$username = "root";
 //$password = "";
 
-$servername = "remotemysql.com";
-$db = "c98l9GRLrq";
-$username = "c98l9GRLrq";
-$password = "usyd2f5kCr";
-$charset = "utf8mb4"
+$host = 'remotemysql.com';
+$db = 'c98l9GRLrq';
+$username = 'c98l9GRLrq';
+$password = 'usyd2f5kCr';
+$charset = 'utf8mb4';
 
-$conn = new PDO("mysql:host=$servername;dbname=$db;charset=$charset");
+$conn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 try {
-    $conn = new PDO($conn, $username, $password);
+    $pdo = new PDO($conn, $username, $password);
     // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
