08386eef7f29c0ad000baf4500daf87544135784
mohammedreda <ridaeljirari@gmail.com>
2022-02-09 23:22:57 +0100

    never give up 3.6

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: vendor/bin/heroku-php-apache2
-web: home.php
+web: vendor/bin/heroku-php-apache2 web/
--- connection.php
+++ connection.php
@@ -9,13 +9,13 @@ $username = 'c98l9GRLrq';
 $password = 'usyd2f5kCr';
 $charset = 'utf8mb4';
 
-$conn = "mysql:host=$host;dbname=$db;charset=$charset";
+$pdo = "mysql:host=$host;dbname=$db;charset=$charset";
 
 try {
-    $pdo = new PDO($conn, $username, $password);
+    $conn = new PDO($conn, $username, $password);
     // set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
