f82d2cf4bba526a5c68097e71a964c0d032c41c2
mohammedreda <ridaeljirari@gmail.com>
2022-02-09 17:44:03 +0100

    please work

--- connection.php
+++ connection.php
@@ -3,13 +3,16 @@
 //$username = "root";
 //$password = "";
 
-$servername = "localhost";
+$servername = "remotemysql.com";
 $db = "c98l9GRLrq";
 $username = "c98l9GRLrq";
 $password = "usyd2f5kCr";
+$charset = "utf8mb4"
+
+$conn = new PDO("mysql:host=$servername;dbname=$db;charset=$charset");
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=c98l9GRLrq", $username, $password);
+    $conn = new PDO($conn, $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
