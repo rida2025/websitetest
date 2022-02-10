ec445e9cce334c9838d23312486e92b78663e72c
mohammedreda <ridaeljirari@gmail.com>
2022-02-10 01:06:31 +0100

    update 3

--- connection.php
+++ connection.php
@@ -3,16 +3,19 @@
 //$username = "root";
 //$password = "";
 
-$host = 'remotemysql.com';
-$db = 'c98l9GRLrq';
-$username = 'c98l9GRLrq';
-$password = 'usyd2f5kCr';
-$charset = 'utf8mb4';
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-$pdo = "mysql:host=$host;dbname=$db;charset=$charset";
 
 try {
-    $conn = new PDO($pdo, $username, $password);
+    $conn = new PDO("mysql:host=$cleardb_server;dbname=heroku_6daa70e44adaa77", $cleardb_username, $cleardb_password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
