f70920fd453834893baf6a819962fb7b7e8277b3
mohammedreda <ridaeljirari@gmail.com>
2022-02-10 01:52:31 +0100

    good update

--- register.php
+++ register.php
@@ -94,7 +94,7 @@ session_start();
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Pages / Register - NiceAdmin Bootstrap Template</title>
+  <title>Register - Rush To Learn</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
